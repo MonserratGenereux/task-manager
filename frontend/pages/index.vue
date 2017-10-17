3b3021dc68f16846d6972694af3fb4d298c449a2
Monserrat Sánchez Genereux <mo_ma1996@hotmail.com>
2017-10-16 23:07:06 -0500

    Frontend (#19)
    Frontend project structure

---
+++ frontend/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+<section>
+Pagina de landing
+<p>{{cuerpo}}</p>
+</section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      cuerpo: 'das'
+    }
+  },
+  mounted: function () {
+    this.getHabits()
+  },
+  methods: {
+    getHabits: function () {
+      axios.get('https://jsonplaceholder.typicode.com/posts', {
+        params: {
+          id: 1
+        }
+      })
+        .then((response) => {
+          console.log(response)
+          this.cuerpo = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
