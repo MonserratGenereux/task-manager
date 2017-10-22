3c7c101dc60644bd5565b0a8543efbad73721963
vic3r <barcelonamane@hotmail.com>
2017-10-21 23:07:38 -0500

    Refix in task schema

--- backend/src/schemas/task.js
+++ backend/src/schemas/task.js
@@ -1,14 +1,14 @@
 module.exports = {
-    habit:{
+    habit: {
         type: 'object',
-        properties:{
-            id: { type: 'string'},
+        properties: {
+            id: { type: 'string' },
             name: { type: 'string' },
-            created: {type: 'DATE'},
-            completedOnTime: {type:'DATE'}
-            completedBeforeTime: {type:'DATE'}
-            dueDate: {type:'DATE'}
-            completed:{type:'BOOLEAN'}
+            created: { type: 'DATE' },
+            completedOnTime: { type: 'DATE' },
+            completedBeforeTime: { type: 'DATE' },
+            dueDate: { type: 'DATE' },
+            completed: { type: 'BOOLEAN' }
         },
         additionalProperties: false
     }
