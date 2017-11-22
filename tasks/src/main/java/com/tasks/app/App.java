353250170c0832033a747df71624d6fc2040b28f
Victor Garcia <barcelonamane@hotmail.com>
2017-11-21 23:04:17 -0600

    Dockerize tasks & configure application.conf (#73)
    * Dockerize tasks & configure application.conf
    
    * Delete production.conf

--- tasks/src/main/java/com/tasks/app/App.java
+++ tasks/src/main/java/com/tasks/app/App.java
@@ -2,51 +2,59 @@ package com.tasks.app;
 
 import io.grpc.BindableService;
 import com.tasks.app.db.PostgreSQL.TasksDatabaseSQL;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import io.grpc.netty.NettyServerBuilder;
+
+import java.io.File;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.logging.Logger;
 
-
 public class App {
-    private static final Logger logger = Logger.getLogger(App.class.getName());
+	private static final Logger logger = Logger.getLogger(App.class.getName());
 
-  private Server server;
-  private TasksDatabaseSQL taskDB;
-  private void start() throws IOException {
+	private Server server;
+	private TasksDatabaseSQL taskDB;
 
-      int port = 50051;
-      taskDB = new TasksDatabaseSQL();
-      server = ServerBuilder.forPort(port)
-        .addService((BindableService) new TasksImpl(taskDB))
-        .build()
-        .start();
-      logger.info("Server started, listening on " + port);
-      Runtime.getRuntime().addShutdownHook(new Thread() {
-          @Override
-          public void run(){
-              // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-            System.err.println("*** shutting down gRPC server since JVM is shutting down");
-            App.this.stop();
-            System.err.println("*** server shut down");
-          }
-      });
-  }
+	private void start() throws IOException {
+		Config conf = ConfigFactory.load();
+		final int port = conf.getInt("server.port");
+		final String host = conf.getString("server.host");
+		taskDB = new TasksDatabaseSQL();
+		server = NettyServerBuilder.forAddress(new InetSocketAddress(host, port)).
+				addService(new TasksImpl(taskDB)).build().start();
+		
+		logger.info("Server started, listening on " + port);
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				// Use stderr here since the logger may have been reset by its
+				// JVM shutdown hook.
+				System.err.println("*** shutting down gRPC server since JVM is shutting down");
+				App.this.stop();
+				System.err.println("*** server shut down");
+			}
+		});
+	}
 
-  private void stop(){
-      if (server != null) {
-        server.shutdown();
-      } 
-  }
-    private void blockUntilShutdown() throws InterruptedException {
-        if (server != null) {
-        server.awaitTermination();
-        }
-    }
-    
-    public static void main( String[] args ) throws IOException, InterruptedException {
-        final App server = new App();
-        server.start();
-        server.blockUntilShutdown();
-    }
+	private void stop() {
+		if (server != null) {
+			server.shutdown();
+		}
+	}
+
+	private void blockUntilShutdown() throws InterruptedException {
+		if (server != null) {
+			server.awaitTermination();
+		}
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException {	
+		final App server = new App();
+		server.start();
+		server.blockUntilShutdown();
+	}
 }
